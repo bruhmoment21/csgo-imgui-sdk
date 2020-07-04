56d832c054c633bbbbd0678826e036d4008f06f3
Marius <53657322+bruhmoment21@users.noreply.github.com>
2020-07-04 15:01:39 +0300

    Update sdk.cpp

--- blicky sdk/src/sdk files/sdk.cpp
+++ blicky sdk/src/sdk files/sdk.cpp
@@ -6,7 +6,5 @@ void interfaces::initialize( ) {
 	surface = get_interface<i_surface>( "vguimatsurface.dll", "VGUI_Surface031" );
 	input_system = get_interface<i_input_system>( "inputsystem.dll", "InputSystemVersion001" );
 	
-	directx = **reinterpret_cast< IDirect3DDevice9*** >( utilities::pattern_scan( GetModuleHandleA( "shaderapidx9.dll" ), "A1 ? ? ? ? 50 8B 08 FF 51 0C" ) + 1 );
-	
 	std::cout << "Interfaces are good!" << std::endl;
 }
