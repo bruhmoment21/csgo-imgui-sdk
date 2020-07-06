d651be1dbd100d1a377cc9c61ad91218ee7f30ea
Marius <53657322+bruhmoment21@users.noreply.github.com>
2020-07-06 18:29:10 +0300

    Update i_surface.hpp

--- blicky sdk/src/sdk files/interfaces/i_surface.hpp
+++ blicky sdk/src/sdk files/interfaces/i_surface.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-class i_surface {
+class i_surface { // don't bother using surface. try to use imgui as much as you can where you can(better performance).
 public:
 	void unlock_cursor( ) {
 		using original_fn = void( __thiscall* )( i_surface* );
