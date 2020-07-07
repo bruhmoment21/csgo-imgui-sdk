b98ec23dbcc5909caa84122be42cbcccc6ec4aab
Marius <53657322+bruhmoment21@users.noreply.github.com>
2020-07-07 21:34:24 +0300

    Update math.cpp

--- blicky sdk/src/other files/math/math.cpp
+++ blicky sdk/src/other files/math/math.cpp
@@ -13,7 +13,7 @@ bool math::world_to_screen( const vec3_t& origin, ImVec2& screen ) {
 
     static std::uintptr_t view_matrix; // engine get_view_matrix will cause flickering if you have multithreading enabled and draw using dx9
     if ( !view_matrix )
-        view_matrix = *reinterpret_cast< std::uintptr_t* >( reinterpret_cast< std::uintptr_t >( utilities::pattern_scan( GetModuleHandleA( "client.dll" ), "0F 10 05 ? ? ? ? 8D 85 ? ? ? ? B9" ) ) + 3 ) + 176;
+        view_matrix = *reinterpret_cast< std::uintptr_t* >( utilities::pattern_scan( "client.dll", "0F 10 05 ? ? ? ? 8D 85 ? ? ? ? B9" ) + 3 ) + 176;
 
     const auto& matrix = *reinterpret_cast< matrix_4x4* >( view_matrix ); // matrix_3x4 works well too since all matrix.m[3][0, 1, 2] values are all 0 and [3] is 1
 
