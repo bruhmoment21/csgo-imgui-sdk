737357d77ec29069a0b5d78d960abb6e7e08d44e
Marius <53657322+bruhmoment21@users.noreply.github.com>
2020-07-07 21:32:45 +0300

    Update utilities.hpp

--- blicky sdk/src/other files/utilities/utilities.hpp
+++ blicky sdk/src/other files/utilities/utilities.hpp
@@ -4,5 +4,5 @@
 namespace utilities {
 	void allocate_console( );
 	void free_console( );
-	std::uint8_t* pattern_scan( void* module, const char* signature );
+	std::uint8_t* pattern_scan( const char* module, const char* signature );
 }
