738097ef15d903abcb7bb85cea073899ec6b0461
Marius <53657322+bruhmoment21@users.noreply.github.com>
2020-07-07 17:49:27 +0300

    Update math.cpp

--- blicky sdk/src/other files/math/math.cpp
+++ blicky sdk/src/other files/math/math.cpp
@@ -15,7 +15,7 @@ bool math::world_to_screen( const vec3_t& origin, ImVec2& screen ) {
     if ( !view_matrix )
         view_matrix = *reinterpret_cast< std::uintptr_t* >( reinterpret_cast< std::uintptr_t >( utilities::pattern_scan( GetModuleHandleA( "client.dll" ), "0F 10 05 ? ? ? ? 8D 85 ? ? ? ? B9" ) ) + 3 ) + 176;
 
-    const auto& matrix = *reinterpret_cast< matrix_4x4* >( view_matrix );
+    const auto& matrix = *reinterpret_cast< matrix_4x4* >( view_matrix ); // matrix_3x4 works well too since all matrix.m[3][0, 1, 2] values are all 0 and [3] is 1
 
     const auto w = matrix.m[ 3 ][ 0 ] * origin.x + matrix.m[ 3 ][ 1 ] * origin.y + matrix.m[ 3 ][ 2 ] * origin.z + matrix.m[ 3 ][ 3 ];
     if ( w < 0.001f )
