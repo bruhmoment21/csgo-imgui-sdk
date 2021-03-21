b13038d38c7a91bd446da6687794065d85c4adf8
Marius <53657322+bruhmoment21@users.noreply.github.com>
2021-03-21 13:29:47 +0200

    unreferenced

--- jet-sdk-csgo/src/config.hpp
+++ jet-sdk-csgo/src/config.hpp
@@ -14,29 +14,5 @@ namespace config
 	inline bool bypass_invite_cooldown;
 	inline bool fake_prime;
 
-	struct item_t
-	{
-		char custom_name[32] = "";
-		char preview_name[128] = "";
-
-		// Used for loading/saving
-		short index = 0;
-		int quality = 0;
-		int paint_kit = 0;
-		float wear = -1.f;
-		int seed = -1;
-		int stattrak_kills = -1;
-		std::uint64_t item_id = 0u;
-		std::uint32_t stickers[5];
-	};
-
-	inline std::vector<item_t> added_items;
-	
-	// Don't save
-	inline int stattrak_kills = -1, seed = -1;
-	inline float wear = 0.f;
-	inline float stattrak_chance = 17.f, maximum_wear_chance = 0.65f, knife_chance = 0.15f;
-	inline bool is_souvenir;
-
 	inline std::vector<std::string> configs;
 }
